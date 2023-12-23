c8066ab07ad0bd45e24497378270d26439b7913e
msr-dev7 <62176407+msr-dev7@users.noreply.github.com>
2023-12-23 15:40:00 -0300

    alteração do texto

--- app.py
+++ app.py
@@ -1,49 +1,51 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Trabalho final da disciplina Machine Learning Hands On",
+    page_title="Machine Learning Hands On",
     page_icon="\u2B50",
     layout="wide",
     initial_sidebar_state="expanded")
 
+st.header('SENAI CIMATEC')
+st.header('ESPECIALIZAÇÃO EM DATA SCIENCE & ANALYTICS')
+st.markdown("---")
+st.title('Machine Learning Hands On')
 
-st.title('Trabalho final da disciplina Machine Learning Hands On')
+st.markdown("---")
+
 
 # Texto de introdução
 st.markdown("""
 ## 
-Reunir em um WebApp Streamlit as tarefas solicitadas em aula, isto é, esse WebApp deve conter as seguintes páginas:
 
-- O deploy de pelo menos um modelo de Machine Learning (vocês fizeram nas aulas passadas).
-- O chatbot com um assistente definido por vocês e com as características solicitadas no exercício. 
-- Uma página 'Sobre', com os nomes dos envolvidos na criação do app e informações adicionais que julgarem pertinentes.
+**Detalhamento do WebApp Streamlit:**
 
-Capriche e descreva toda a funcionalidade do seu WebApp para o usuário! 
+**- Deploy do modelo 1 e do modelo 2 de Machine Learning para doenças cardíacas**
 
-Utilize com criatividade os recursos do Streamlit. Deixe seu app pronto para que um usuário qualquer possa utilizá-lo!
+O usuário deve informar os dados solicitados da pessoa e clicar no botão da aplicação que realizará previsões 
+sobre o risco de doenças cardíacas. Este recurso utiliza um modelo de machine learning avançado, alimentado por 
+características e dados pessoais, treinado com uma vasta gama de informações médicas.
 
-O trabalho pode ser feito em grupo de até 3 pessoas.
+**- Chatbot com um assistente que conversará sobre a história do Brasil** 
 
-Submeter o trabalho no link de submissão: https://forms.gle/s1cADrEATWjcmcVt6
+Para realizar apenas uma simples conversa com o assistente, o usuário deve informar apenas a chave da API OpenAI.
 
+Para utilizar o conhecimento de um arquivo PDF, o usuário deve informar a chave da API OpenAI, a chave do Pinecone e um arquivo PDF com dados mais recentes da história do Brasil.
+Como sugestão temos o [PDF Retrospectiva de 2023](https://drive.google.com/file/d/117PS69h2xIQ1K7S0x8JIoy0ABKioJwy2/view?usp=sharing), que foi extraído do site [Startupi](https://startupi.com.br/retrospectiva-2023/).
 
-Assistente Virtual:
+É possível ainda configurar diretamente no aplicativo o nível de criatividade, o tamanho e o estilo das respostas do assistente virtual. 
+Ao finalizar a conversa clicando diretamente no botão específico, o usuário obterá todo o histórico conversado em um arquivo PDF, 
+disponibilizado para download na aplicação.
 
-Crie um assistente pessoal com instruções específicas para um problema de sua escolha. 
-Inclua opções ao usuário para controlar a criatividade da resposta, o tamanho da resposta e o estilo de escrita da resposta. 
-Adicione mais, ou outras, opções como widgets de interação com usuário se desejar.
+**- Chat por voz - Bônus**
 
-No uso do assistente, inclua um escape de moderação, isto é, caso o input do usuário violar as regras (você define), 
-uma resposta automática deve ser dada, ao invés de acionar a API de chat.
+O usuário deve clicar no botão específico para iniciar a interação por voz, realizar o diálogo por voz com o assistente e, quando desejar, 
+finalizar o diálogo clicando no botão específico.
 
-Faça com que o programa tenha uma opção para 'finalizar a conversa' e quando isso acontecer, um conjunto de dados (pandas mesmo) 
-deve ser alimentado com as seguintes informações: dia e horário da conversa, tokens utilizados na conversa, custo estimado da conversa, 
-histórico do diálogo.
+**- Sobre**
 
-Além disso, gere um arquivo em PDF com o histórico da conversa e permita ao usuário baixar esse arquivo.
+Dados básicos do curso e da equipe. 
 
-Complemento: adicione o modo RAG na conversa, isto é, permita ao usuário subir um arquivo de texto e que a informação do arquivo seja 
-levada em consideração na conversa com o chatbot. 
-
-Exercício Bônus [NÃO OBRIGATÓRIO] - Crie um app para interagir por voz com o ChatGPT
 """)
+
+st.markdown("---")

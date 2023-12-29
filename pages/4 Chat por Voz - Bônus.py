ec0158225929c2f21dc750106c41c502cad20678
msr-dev7 <62176407+msr-dev7@users.noreply.github.com>
2023-12-29 03:18:29 -0300

    Update 4 Chat por Voz - Bônus.py

--- pages/4 Chat por Voz - Bônus.py
+++ pages/4 Chat por Voz - Bônus.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import speech_recognition as sr
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
 from gtts import gTTS
 import os
 
-#openai_api_key = st.sidebar.text_input('Chave da API OpenAI', type = 'password')
-openai_api_key = st.secrets["my_secret"]["OPENAI_API_KEY"]
 if openai_api_key:
-    llm = OpenAI(api_key=openai_api_key, temperature = 0)
-    # Função para reconhecimento de fala
+    chat = ChatOpenAI(api_key=openai_api_key)
+
+    llm = ConversationChain(llm=chat)
+
     def recognize_speech():
         recognizer = sr.Recognizer()
         with sr.Microphone() as source:
@@ -25,7 +26,7 @@ if openai_api_key:
     # Função para interagir com o ChatGPT
     def chat_with_gpt(prompt):
         try:
-            saida = llm(prompt)
+            saida = llm.run(prompt)
             return saida
         except Exception as e:
             return f"Erro ao interagir com o modelo: {str(e)}"
@@ -36,32 +37,44 @@ if openai_api_key:
         tts.save("response.mp3")
         os.system("mpg321 response.mp3")
 
+
+
     # Interface do Streamlit
     st.header("Chat por Voz")
 
-    # Botão para iniciar a interação por voz
     c0,_,c1 = st.columns(3)
+    
     with c0:
-        inicio = st.button("Iniciar Interação por Voz")
+        inicio = st.button("Iniciar Interacao por Voz")
+
     with c1:
-        stop_button = st.button("Encerrar Conversa", key="encerrar_conversa")            
+        stop_button = st.button("Encerrar Conversa", key="encerrar_conversa")
 
-    # Antes do loop while
-    conversa = []  # Lista para armazenar o histórico da conversa
-    interagindo = False  # Variável de controle para interação
+    
+    if "mensagens" not in st.session_state:
+        st.session_state.mensagens = [{"role": 'system', "content": 'Você será um amigo para conversar qualquer coisa.'}] 
 
-    # Durante a interação
+    interagindo = False
+    # Restante do seu código
+    
     if inicio:
         interagindo = True
+
 
     while interagindo:
         input_text = recognize_speech()
         if input_text and "Desculpe, não entendi o áudio." not in input_text:
-            conversa.append(("Usuário:", input_text))  # Adiciona entrada do usuário ao histórico
-            st.write("Usuário:", input_text)
+            #conversa.append(("Usuário:", input_text))  # Adiciona entrada do usuário ao histórico
+            st.session_state.mensagens.append({"role": 'user', "content": input_text})
+            #st.write(f"Transcrição do áudio: {input_text}")
+            with st.chat_message("user"):
+                    st.markdown(input_text)
             response_text = chat_with_gpt(input_text)
-            conversa.append(("ChatGPT:", response_text))  # Adiciona resposta do modelo ao histórico
-            st.write("ChatGPT:", response_text)
+            #conversa.append(("Assistente:", response_text))  # Adiciona resposta do modelo ao histórico
+            st.session_state.mensagens.append({"role": 'system', "content": response_text})
+            #st.write(f"Assistente: {response_text}")
+            with st.chat_message("system"):
+                    st.markdown(response_text)
             if "Erro ao interagir com o modelo" not in response_text:
                 text_to_speech(response_text)
                 st.audio("response.mp3")
@@ -71,12 +84,13 @@ if openai_api_key:
         if stop_button:
             interagindo = False  # Encerra a interação
 
-    # Após o encerramento da conversa
-    st.write("Histórico da Conversa:")
-    for entry in conversa:
-        if "ChatGPT:" in entry[0]:  # Verifica se é uma resposta do modelo para formatar a exibição
-            st.text(entry[0] + " " + entry[1])
-        else:
-            st.write(entry[0], entry[1])  # Mostra entrada do usuário
-else:
-    st.warning("Por favor, informe a chave da API OpenAI.")                
+
+    if st.session_state.mensagens != [{"role": 'system', "content": 'Você será um amigo para conversar qualquer coisa.'}]:            
+            st.markdown("Histórico da Conversa:")
+    
+    for mensagens in st.session_state.mensagens[1:]:
+        with st.chat_message(mensagens["role"]):
+            st.markdown(mensagens["content"])
+    
+    st.session_state.mensagens = [{"role": 'system', "content": 'Você será um amigo para conversar qualquer coisa.'}]             
+    
